b918d74e5e16ff21fc0c34c43138913a330f9110
geoffrey45 <geoffreymungai45@gmail.com>
2020-12-28 18:18:45 +0300

    check whether post exists

--- app/api/views.py
+++ app/api/views.py
@@ -8,18 +8,23 @@ post_parser.add_argument('post_body', help='This field cannot be blank!')
 
 @api.route('/')
 def index():
-    return {'message':'success!'}
+    return {'msg':'success!'}
 
 class AddNewPost(Resource):
     def post(self):
         data = post_parser.parse_args()
 
         if data['post_body'] == '' or None:
-            return {'msg': 'post cannot be blank'}, 401
+            return {'msg': 'blank post not allowed'}, 401
+
+        post_exists = post_instance.get_post_by_body(data['post_body'])
+        if post_exists:
+            return {'msg': 'already exists'}
 
         new_post_data = {
             'post_body': data['post_body']
         }
+
         try:
             post_instance.save(new_post_data)
             return {'msg': 'Published!'}, 201
