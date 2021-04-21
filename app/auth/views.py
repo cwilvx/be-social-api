7ad0e72e7d01c43579c9e2892754bda804cdf12d
Restyled.io <commits@restyled.io>
2021-04-21 13:19:52 +0000

    Restyled by reorder-python-imports

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,10 +2,13 @@ import datetime
 import json
 
 from bson import json_util
-from flask_jwt_extended import (create_access_token, create_refresh_token,
-                                get_jwt_identity, jwt_refresh_token_required,
-                                jwt_required)
-from flask_restful import Resource, reqparse
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_refresh_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_refresh_token_required
+from flask_jwt_extended import jwt_required
+from flask_restful import reqparse
+from flask_restful import Resource
 
 from app.models import Users
 
