c550942aa6cccb6499f2606fd282654cd3921d66
Restyled.io <commits@restyled.io>
2021-04-26 13:08:22 +0000

    Restyled by black

--- app/api/views.py
+++ app/api/views.py
@@ -11,19 +11,19 @@ from . import api
 
 post_instance = Posts()
 post_parser = reqparse.RequestParser()
-post_parser.add_argument('post_body', help='This field cannot be blank!')
-post_parser.add_argument('post_id')
-post_parser.add_argument('tags', action="append")
+post_parser.add_argument("post_body", help="This field cannot be blank!")
+post_parser.add_argument("post_id")
+post_parser.add_argument("tags", action="append")
 
 
 def generate_post_id():
-    post_id = ''.join(random.choice(string.ascii_letters) for i in range(10))
+    post_id = "".join(random.choice(string.ascii_letters) for i in range(10))
     return post_id
 
 
-@api.route('/wp-admin')
+@api.route("/wp-admin")
 def index():
-    return {'msg': 'See no evil! 🙈'}
+    return {"msg": "See no evil! 🙈"}
 
 
 class AddNewPost(Resource):
@@ -32,28 +32,27 @@ class AddNewPost(Resource):
         data = post_parser.parse_args()
         current_user = get_jwt_identity()
 
-        if data['post_body'] == '' or None:
-            return {'msg': 'blank post not allowed'}, 401
+        if data["post_body"] == "" or None:
+            return {"msg": "blank post not allowed"}, 401
 
         # post_exists = post_instance.get_post_by_body(data['post_body'])
         # if post_exists:
         #     return {'msg': 'already exists'}
 
         new_post_data = {
-            'post_body': data['post_body'],
-            'user': current_user['user_id'],
-            'tags': data['tags'],
-            'post_id': generate_post_id()
+            "post_body": data["post_body"],
+            "user": current_user["user_id"],
+            "tags": data["tags"],
+            "post_id": generate_post_id(),
         }
 
         try:
             post_instance.save(new_post_data)
-            post_data = json.loads(json.dumps(
-                new_post_data, default=json_util.default))
+            post_data = json.loads(json.dumps(new_post_data, default=json_util.default))
 
             return post_data, 201
         except:
-            return {'msg': 'Something went wrong'}, 500
+            return {"msg": "Something went wrong"}, 500
 
 
 class AllPosts(Resource):
@@ -72,7 +71,7 @@ class SinglePost(Resource):
     @staticmethod
     def post():
         data = post_parser.parse_args()
-        post_id = data['post_id']
+        post_id = data["post_id"]
 
         if post_id:
             post = post_instance.get_post_by_id(post_id)
@@ -98,7 +97,7 @@ class DeletePost(Resource):
         """
         current_user = get_jwt_identity()
         data = post_parser.parse_args()
-        post_id = data['post_id']
+        post_id = data["post_id"]
 
         try:
             if post_id:
@@ -107,14 +106,14 @@ class DeletePost(Resource):
                 if post is None:
                     return {"msg": "Post does not exist"}, 404
 
-                if current_user['user_id'] == post['user']:
+                if current_user["user_id"] == post["user"]:
                     try:
                         post_instance.delete_post(post_id)
-                        return {'msg': 'Post deleted successfully'}, 410
+                        return {"msg": "Post deleted successfully"}, 410
                     except:
-                        return {'msg': 'An exception occurred'}, 500
+                        return {"msg": "An exception occurred"}, 500
                 else:
-                    return {'msg': 'Permission denied'}, 403
+                    return {"msg": "Permission denied"}, 403
             else:
                 return {"msg": "post_id is required!"}
         except:
--- manage.py
+++ manage.py
@@ -1,9 +1,7 @@
 import os
 from app import create_app
 
-app = create_app("{mode}".format(
-    mode=os.environ.get('MODE')
-))
+app = create_app("{mode}".format(mode=os.environ.get("MODE")))
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
