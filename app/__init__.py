453e586f9b203cf588fe714130125be8c9e6312b
Mungai Geoffrey <geoffreymungai45@gmail.com>
2021-04-26 15:03:14 +0300

    Implement searching posts (#16)
    Also fix get post by ObjectId()

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
-from flask_restful import Api
-from flask_jwt_extended import JWTManager
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+from flask_restful import Api
 
+from . import api
+from . import auth
 from config import config_options
-from . import api, auth
 
 rest = Api()
 jwt = JWTManager()
@@ -17,25 +18,27 @@ def create_app(config_name):
 
     rest.init_app(app)
     jwt.init_app(app)
-    cors = CORS(app, resources={r'/*': {'origins': '*'}})
 
     from .auth import auth as auth_blueprint
+
     app.register_blueprint(auth_blueprint)
 
     from .api import api as api_blueprint
+
     app.register_blueprint(api_blueprint)
 
     return app
 
 
-rest.add_resource(api.views.AllPosts, '/')
-rest.add_resource(api.views.AddNewPost, '/posts/new')
-rest.add_resource(auth.views.UserRegistration, '/auth/signup')
-rest.add_resource(auth.views.UserLogin, '/auth/login')
-rest.add_resource(auth.views.TokenRefresh, '/auth/token/refresh')
-rest.add_resource(api.views.SinglePost, '/posts/single')
-rest.add_resource(api.views.DeletePost, '/posts/delete')
-rest.add_resource(auth.views.GetCurrentUser, '/auth/user')
+rest.add_resource(api.views.AllPosts, "/")
+rest.add_resource(api.views.AddNewPost, "/posts/new")
+rest.add_resource(auth.views.UserRegistration, "/auth/signup")
+rest.add_resource(auth.views.UserLogin, "/auth/login")
+rest.add_resource(auth.views.TokenRefresh, "/auth/token/refresh")
+rest.add_resource(api.views.SinglePost, "/posts/single")
+rest.add_resource(api.views.DeletePost, "/posts/delete")
+rest.add_resource(auth.views.GetCurrentUser, "/auth/user")
+rest.add_resource(api.views.SearchPosts, "/posts/search")
 
 
 @jwt.user_identity_loader
--- app/api/views.py
+++ app/api/views.py
@@ -1,29 +1,26 @@
 import json
-import random
-import string
 
 from bson import json_util
-from flask_jwt_extended import get_jwt_identity, jwt_required
-from flask_restful import Resource, reqparse
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_restful import reqparse
+from flask_restful import Resource
 
-from app.models import Posts
 from . import api
+from app.models import Posts
 
 post_instance = Posts()
 post_parser = reqparse.RequestParser()
-post_parser.add_argument('post_body', help='This field cannot be blank!')
-post_parser.add_argument('post_id')
-post_parser.add_argument('tags', action="append")
+post_parser.add_argument("post_body", help="This field cannot be blank!")
+post_parser.add_argument("post_id")
+post_parser.add_argument("tags", action="append")
+
+post_parser.add_argument("q", help="This field cannot be blank!")
 
 
-def generate_post_id():
-    post_id = ''.join(random.choice(string.ascii_letters) for i in range(10))
-    return post_id
-
-
-@api.route('/wp-admin')
+@api.route("/wp-admin")
 def index():
-    return {'msg': 'See no evil! 🙈'}
+    return {"msg": "See no evil! 🙈"}
 
 
 class AddNewPost(Resource):
@@ -33,32 +30,33 @@ class AddNewPost(Resource):
         data = post_parser.parse_args()
         current_user = get_jwt_identity()
 
-        if data['post_body'] == '' or None:
-            return {'msg': 'blank post not allowed'}, 401
+        if data["post_body"] == "" or None:
+            return {"msg": "blank post not allowed"}, 401
 
         # post_exists = post_instance.get_post_by_body(data['post_body'])
         # if post_exists:
         #     return {'msg': 'already exists'}
 
         new_post_data = {
-            'post_body': data['post_body'],
-            'user': current_user['user_id'],
-            'tags': data['tags'],
-            'post_id': generate_post_id()
+            "user": current_user["user_id"],
+            "post_body": data["post_body"],
+            "tags": data["tags"],
         }
 
         try:
             post_instance.save(new_post_data)
-            post_data = json.loads(json.dumps(new_post_data, default=json_util.default))
+            post_data = json.loads(
+                json.dumps(new_post_data, default=json_util.default))
 
             return post_data, 201
         except:
-            return {'msg': 'Something went wrong'}, 500
+            return {"msg": "Something went wrong"}, 500
 
 
 class AllPosts(Resource):
-    @staticmethod
-    def get():
+    # @staticmethod
+    def get(self):
+
         all_posts = []
         posts = post_instance.get_all_posts()
         for post in posts:
@@ -73,7 +71,7 @@ class SinglePost(Resource):
     @staticmethod
     def post():
         data = post_parser.parse_args()
-        post_id = data['post_id']
+        post_id = data["post_id"]
 
         if post_id:
             post = post_instance.get_post_by_id(post_id)
@@ -91,16 +89,15 @@ class SinglePost(Resource):
 
 class DeletePost(Resource):
     @jwt_required
-    @staticmethod
-    def post():
-        """Deletes a post
+    def post(self):
+        """Deletes a post.
 
         Returns:
             410 (status)
         """
         current_user = get_jwt_identity()
         data = post_parser.parse_args()
-        post_id = data['post_id']
+        post_id = data["post_id"]
 
         try:
             if post_id:
@@ -108,19 +105,33 @@ class DeletePost(Resource):
 
                 if post is None:
                     return {"msg": "Post does not exist"}, 404
-                else:
-                    post_obj = json.dumps(post, default=json_util.default)
-                    post_item = json.loads(post_obj)
 
-                    if current_user['user_id'] == post['user']:
-                        try:
-                            post_instance.delete_post(post_id)
-                            return {'msg': 'Post deleted successfully'}, 410
-                        except:
-                            return {'msg': 'An exception occurred'}, 500
-                    else:
-                        return {'msg': 'Permission denied'}, 403
+                if current_user["user_id"] == post["user"]:
+                    try:
+                        post_instance.delete_post(post_id)
+                        return {"msg": "Post deleted successfully"}, 410
+                    except:
+                        return {"msg": "An exception occurred"}, 500
+                else:
+
+                    return {"msg": "Permission denied"}, 403
             else:
                 return {"msg": "post_id is required!"}
         except:
             return {"msg": "Something went wrong!"}, 500
+
+
+class SearchPosts(Resource):
+    @staticmethod
+    def get():
+        data = post_parser.parse_args()
+        query = data["q"]
+
+        query_results = []
+        posts = post_instance.search_post_body(query)
+        for post in posts:
+            post_obj = json.dumps(post, default=json_util.default)
+            post_item = json.loads(post_obj)
+            query_results.append(post_item)
+
+        return query_results
